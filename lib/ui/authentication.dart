061e2d442568ff113dffda6940712faa871eb671
Beast-Sanjay <sanjuniky2002@gmail.com>
2022-07-22 22:23:36 +0530

    authentication is created

---
+++ lib/ui/authentication.dart
@@ -0,0 +1,129 @@
+import 'package:hospicare_app/firebase_auth/flutterfire.dart';
+import 'package:flutter/material.dart';
+
+import 'home_view.dart';
+
+class Authentication extends StatefulWidget {
+  Authentication({Key? key}) : super(key: key);
+
+  @override
+  _AuthenticationState createState() => _AuthenticationState();
+}
+
+class _AuthenticationState extends State<Authentication> {
+  TextEditingController _emailField = TextEditingController();
+  TextEditingController _passwordField = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          color: Color.fromARGB(255, 61, 227, 230),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              child: Text(
+                'Login for Hospicare',
+                style: TextStyle(
+                    //fontFamily: 'TrajanPro'
+                    color: Colors.grey[800],
+                    fontWeight: FontWeight.w900,
+                    fontStyle: FontStyle.normal,
+                    fontFamily: 'SourceCodePro',
+                    fontSize: 30),
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width / 1.3,
+              child: TextFormField(
+                style: TextStyle(color: Colors.white),
+                controller: _emailField,
+                decoration: InputDecoration(
+                  hintText: "something@email.com",
+                  hintStyle: TextStyle(
+                    color: Colors.white,
+                  ),
+                  labelText: "Email",
+                  labelStyle: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height / 35),
+            Container(
+              width: MediaQuery.of(context).size.width / 1.3,
+              child: TextFormField(
+                style: TextStyle(color: Colors.white),
+                controller: _passwordField,
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "password",
+                  hintStyle: TextStyle(
+                    color: Colors.white,
+                  ),
+                  labelText: "Password",
+                  labelStyle: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height / 35),
+            Container(
+              width: MediaQuery.of(context).size.width / 1.4,
+              height: 45,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15.0),
+                color: Colors.white,
+              ),
+              child: MaterialButton(
+                onPressed: () async {
+                  bool shouldNavigate =
+                      await register(_emailField.text, _passwordField.text);
+                  if (shouldNavigate) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => HomeView(),
+                      ),
+                    );
+                  }
+                },
+                child: Text("Register"),
+              ),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height / 35),
+            Container(
+              width: MediaQuery.of(context).size.width / 1.4,
+              height: 45,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15.0),
+                color: Colors.white,
+              ),
+              child: MaterialButton(
+                  onPressed: () async {
+                    bool shouldNavigate =
+                        await signIn(_emailField.text, _passwordField.text);
+                    if (shouldNavigate) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => HomeView(),
+                        ),
+                      );
+                    }
+                  },
+                  child: Text("Login")),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/ui/home_view.dart
@@ -0,0 +1,47 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  HomeView({Key? key}) : super(key: key);
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: Text(
+          "Hospicare ( Username )",
+          style: TextStyle(
+              //fontFamily: 'TrajanPro'
+              color: Colors.grey[800],
+              fontWeight: FontWeight.w900,
+              fontStyle: FontStyle.normal,
+              fontFamily: 'SourceCodePro',
+              fontSize: 20),
+        )),
+        body: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+          ),
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Center(
+            child: Text(
+              'Welcome to Hospicare',
+              style: TextStyle(
+                  //fontFamily: 'TrajanPro'
+                  color: Colors.grey[800],
+                  fontWeight: FontWeight.w900,
+                  fontStyle: FontStyle.normal,
+                  fontFamily: 'SourceCodePro',
+                  fontSize: 30),
+            ),
+          ),
+        ));
+  }
+}
