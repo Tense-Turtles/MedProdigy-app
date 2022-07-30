84260c8effb56eb0492fe5f2d5ba93c4314c7877
Beast-Sanjay <sanjuniky2002@gmail.com>
2022-07-31 00:45:15 +0530

    changed to new code

--- lib/ui/home_view.dart
+++ lib/ui/home_view.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: library_private_types_in_public_api, unused_import, duplicate_ignore
+// ignore_for_file: library_private_types_in_public_api, unused_import, duplicate_ignore, prefer_const_constructors
 
 // ignore: unused_import
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -14,39 +14,40 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  int currentIndex = 0;
+  final screens = [
+    Center(child: Text('Home')),
+    Center(child: Text('Health')),
+    Center(child: Text('Profile')),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: Text(
-          "Hospicare ( Username )",
-          style: TextStyle(
-              //fontFamily: 'TrajanPro'
-              color: Colors.grey[800],
-              fontWeight: FontWeight.w900,
-              fontStyle: FontStyle.normal,
-              fontFamily: 'SourceCodePro',
-              fontSize: 20),
-        )),
-        body: Container(
-          // ignore: prefer_const_constructors
-          decoration: BoxDecoration(
-            color: Colors.white,
-          ),
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          child: Center(
-            child: Text(
-              'Welcome to Hospicare',
-              style: TextStyle(
-                  //fontFamily: 'TrajanPro'
-                  color: Colors.grey[800],
-                  fontWeight: FontWeight.w900,
-                  fontStyle: FontStyle.normal,
-                  fontFamily: 'SourceCodePro',
-                  fontSize: 30),
-            ),
-          ),
-        ));
+      appBar: AppBar(
+        title: Text('HOSPICARE'),
+        elevation: 0,
+        centerTitle: true,
+      ),
+      body: screens[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (index) => setState(
+          () => currentIndex = index,
+        ),
+        iconSize: 35,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'
+              // backgroundColor: Colors.blue[300],
+              ),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'Health'
+              // backgroundColor: Colors.blue[300],
+              ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'
+              // backgroundColor: Colors.blue[300],
+              ),
+        ],
+      ),
+    );
   }
 }
