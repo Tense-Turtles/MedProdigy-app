cb4e5070f3610a593165242f0c4b18876a12d5ea
Beast-Sanjay <sanjuniky2002@gmail.com>
2022-08-03 00:26:42 +0530

    proper sizing

--- lib/ui/register.dart
+++ lib/ui/register.dart
@@ -39,7 +39,7 @@ class _RegisterState extends State<Register> {
                 // mainAxisAlignment: MainAxisAlignment.center,
                 child: Container(
                   padding: EdgeInsets.only(
-                      top: MediaQuery.of(context).size.height * 0.18),
+                      top: MediaQuery.of(context).size.height * 0.1),
 // heading
                   child: Column(
                     //mainAxisAlignment: MainAxisAlignment.center,
