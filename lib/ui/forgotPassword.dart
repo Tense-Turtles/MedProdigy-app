9d2818617644f1ffd8889d4ae333d9ceda65f2eb
Beast-Sanjay <sanjuniky2002@gmail.com>
2022-08-03 00:22:50 +0530

    fixed datatype

--- lib/ui/forgotPassword.dart
+++ lib/ui/forgotPassword.dart
@@ -1,10 +1,15 @@
+// if you are giving your original emailID then please check the spam folder
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:hospicare_app/ui/authentication.dart';
 import 'package:flutter/material.dart';
 
 class ForgetPassword extends StatefulWidget {
+  const ForgetPassword({Key? key}) : super(key: key);
+
   // static const routeName = '/for-pass';
   @override
+  // ignore: library_private_types_in_public_api
   _ForgetPasswordState createState() => _ForgetPasswordState();
 }
 
@@ -13,17 +18,15 @@ class _ForgetPasswordState extends State<ForgetPassword> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+// Starting the body
         body: Container(
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height,
       // ignore: prefer_const_constructors
-      decoration: BoxDecoration(color: Color.fromARGB(255, 255, 255, 255)
-          //image: DecorationImage(
-          //image: AssetImage('assets/main.png'), fit: BoxFit.cover),
-          // ignore: prefer_const_constructors
-          ),
-      padding: EdgeInsets.symmetric(horizontal: 30),
+      decoration: BoxDecoration(color: Color.fromARGB(255, 255, 255, 255)),
+      padding: const EdgeInsets.symmetric(horizontal: 30),
       alignment: Alignment.topCenter,
+// Forgot Password Heading
       child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
         Column(
           children: [
@@ -39,6 +42,7 @@ class _ForgetPasswordState extends State<ForgetPassword> {
                 textAlign: TextAlign.left,
               ),
             ),
+//email Text field
             Container(
               child: TextFormField(
                 validator: (val) {
@@ -51,25 +55,25 @@ class _ForgetPasswordState extends State<ForgetPassword> {
                 controller: email,
                 decoration: const InputDecoration(
                   hintText: "something@email.com",
-                  hintStyle: const TextStyle(
+                  hintStyle: TextStyle(
                     color: Color.fromARGB(255, 116, 116, 116),
                   ),
                   labelText: "Email",
-                  labelStyle: const TextStyle(
-                    color: const Color.fromARGB(255, 0, 0, 0),
+                  labelStyle: TextStyle(
+                    color: Color.fromARGB(255, 0, 0, 0),
                   ),
                 ),
               ),
             ),
           ],
         ),
-        SizedBox(
+// Reset Button
+        const SizedBox(
           height: 10,
         ),
         Container(
           child: MaterialButton(
             onPressed: () async {
-              //  AuthMethods authMethods = AuthMethods();
               FirebaseAuth.instance
                   .sendPasswordResetEmail(
                     email: email.text,
@@ -93,17 +97,13 @@ class _ForgetPasswordState extends State<ForgetPassword> {
               width: MediaQuery.of(context).size.width * 0.5,
               height: 50.0,
               decoration: const BoxDecoration(
-                gradient: const LinearGradient(colors: [
+                gradient: LinearGradient(colors: [
                   Color.fromARGB(255, 255, 136, 34),
                   Color.fromARGB(255, 255, 177, 41)
                 ]),
                 borderRadius: BorderRadius.all(Radius.circular(8.0)),
               ),
               child: Container(
-                // constraints: const BoxConstraints(
-                //     // minWidth: 88.0,
-
-                //     minHeight: 36.0), // min sizes for Material buttons
                 alignment: Alignment.center,
                 child: const Text(
                   "Reset Password",
