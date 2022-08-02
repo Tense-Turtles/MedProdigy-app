9da8ec9705794173bd7be7d38e8b90077f9c9298
Shell Raiser <ksshailesh05@gmail.com>
2022-08-02 20:34:39 +0530

    add card placeholders for graphs

--- lib/ui/home_view.dart
+++ lib/ui/home_view.dart
@@ -15,27 +15,141 @@ class HomeView extends StatefulWidget {
 
 class _HomeViewState extends State<HomeView> {
   int currentIndex = 0;
-  final screens = [
-    Center(child: Text('Home')),
-    Center(child: Text('Health')),
-    Center(child: Text('Profile')),
-  ];
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    var screens = [
+      ListView(children: <Widget>[
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 30),
+          width: double.infinity,
+          // height: double.infinity,
+          child: Center(child: Text('Home')),
+        ),
+        SizedBox(height: 200),
+        Center(
+            child: Row(
+          children: <Widget>[
+            Card(
+              color: Colors.teal[300],
+              child: InkWell(
+                splashColor: Colors.blue.withAlpha(30),
+                onTap: () {
+                  debugPrint('Card tapped.');
+                },
+                child: SizedBox(
+                  // width: double.infinity,
+                  width: size.width * 0.31,
+                  height: 150,
+                  child: Center(child: Text('1st card')),
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.teal[200],
+              child: InkWell(
+                splashColor: Colors.blue.withAlpha(30),
+                onTap: () {
+                  debugPrint('Card tapped.');
+                },
+                child: SizedBox(
+                  width: size.width * 0.31,
+                  // width: double.infinity,
+                  height: 150,
+                  child: Center(child: Text('2nd card')),
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.teal[300],
+              child: InkWell(
+                splashColor: Colors.blue.withAlpha(30),
+                onTap: () {
+                  debugPrint('Card tapped.');
+                },
+                child: SizedBox(
+                  width: size.width * 0.31,
+                  // width: double.infinity,
+                  height: 150,
+                  child: Center(child: Text('3rd card')),
+                ),
+              ),
+            )
+          ],
+        )),
+        Center(
+            child: Row(
+          children: <Widget>[
+            Card(
+              color: Colors.teal[300],
+              child: InkWell(
+                splashColor: Colors.blue.withAlpha(30),
+                onTap: () {
+                  debugPrint('Card tapped.');
+                },
+                child: SizedBox(
+                  // width: double.infinity,
+                  width: size.width * 0.31,
+                  height: 150,
+                  child: Center(child: Text('4th card')),
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.teal[200],
+              child: InkWell(
+                splashColor: Colors.blue.withAlpha(30),
+                onTap: () {
+                  debugPrint('Card tapped.');
+                },
+                child: SizedBox(
+                  width: size.width * 0.31,
+                  // width: double.infinity,
+                  height: 150,
+                  child: Center(child: Text('5th card')),
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.teal[300],
+              child: InkWell(
+                splashColor: Colors.blue.withAlpha(30),
+                onTap: () {
+                  debugPrint('Card tapped.');
+                },
+                child: SizedBox(
+                  width: size.width * 0.31,
+                  // width: double.infinity,
+                  height: 150,
+                  child: Center(child: Text('6th card')),
+                ),
+              ),
+            )
+          ],
+        )),
+      ]),
+      Center(child: Text('Health')),
+      Center(child: Text('Profile')),
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: Text('HOSPICARE'),
         elevation: 0,
         centerTitle: true,
       ),
-      body: screens[currentIndex],
+      body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: screens[currentIndex]),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: currentIndex,
         onTap: (index) => setState(
           () => currentIndex = index,
         ),
         iconSize: 35,
+
         // ignore: prefer_const_literals_to_create_immutables
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'
