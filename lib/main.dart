fa3b3d6de71b331579885799dd2d8ab7304ef811
Beast-Sanjay <sanjuniky2002@gmail.com>
2022-09-02 11:59:33 +0530

    splash screen removed

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hospicare_app/ui/home_view.dart';
-import 'package:hospicare_app/ui/splashScreen.dart';
 import 'firebase_options.dart';
 
 void main() async {
