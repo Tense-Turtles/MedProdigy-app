6515c73cb3c51e9f4749162b9f966bf6f3906f6f
thanmaisai <thanmaisayee123@gmail.com>
2022-09-05 07:14:39 +0530

    added a bluetooth text button

--- lib/ui/home_view.dart
+++ lib/ui/home_view.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: library_private_types_in_public_api, unused_import, duplicate_ignore, prefer_const_constructors, sized_box_for_whitespace,prefer_const_literals_to_create_immutables, import_of_legacy_library_into_null_safe, prefer_const_constructors_in_immutables
+// ignore_for_file: library_private_types_in_public_api, unused_import, duplicate_ignore, prefer_const_constructors, sized_box_for_whitespace,prefer_const_literals_to_create_immutables, import_of_legacy_library_into_null_safe, prefer_const_constructors_in_immutables, unused_local_variable
 
 // ignore: unused_import
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -23,189 +23,246 @@ class _HomeViewState extends State<HomeView> {
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    var screens = [
-      ListView(children: <Widget>[
-        Container(
-          padding: const EdgeInsets.symmetric(vertical: 30),
-          width: double.infinity,
-          // height: double.infinity,
-          child: Center(child: Text('Hello Sriram')),
-        ),
-        SizedBox(height: 50),
-        Center(
-            child: Row(
-          children: <Widget>[
-            Card(
-              color: Colors.teal[300],
-              child: InkWell(
-                splashColor: Colors.blue.withAlpha(30),
-                onTap: () {
-                  debugPrint('Card tapped.');
-                },
-                child: SizedBox(
-                  // width: double.infinity,
-                  width: size.width * 0.31,
-                  height: 150,
-                  child: Center(child: Text('1st card')),
-                ),
-              ),
-            ),
-            Card(
-              color: Colors.teal[200],
-              child: InkWell(
-                splashColor: Colors.blue.withAlpha(30),
-                onTap: () {
-                  debugPrint('Card tapped.');
-                },
-                child: SizedBox(
-                  width: size.width * 0.31,
-                  // width: double.infinity,
-                  height: 150,
-                  child: Center(child: Text('2nd card')),
-                ),
-              ),
-            ),
-            Card(
-              color: Colors.teal[300],
-              child: InkWell(
-                splashColor: Colors.blue.withAlpha(30),
-                onTap: () {
-                  debugPrint('Card tapped.');
-                },
-                child: SizedBox(
-                  width: size.width * 0.31,
-                  // width: double.infinity,
-                  height: 150,
-                  child: Center(child: Text('3rd card')),
-                ),
-              ),
-            )
-          ],
-        )),
-        Center(
-            child: Row(
-          children: <Widget>[
-            Card(
-              color: Colors.teal[300],
-              child: InkWell(
-                splashColor: Colors.blue.withAlpha(30),
-                onTap: () {
-                  debugPrint('Card tapped.');
-                },
-                child: SizedBox(
-                  // width: double.infinity,
-                  width: size.width * 0.31,
-                  height: 150,
-                  child: Center(child: Text('4th card')),
-                ),
-              ),
-            ),
-            Card(
-              color: Colors.teal[200],
-              child: InkWell(
-                splashColor: Colors.blue.withAlpha(30),
-                onTap: () {
-                  debugPrint('Card tapped.');
-                },
-                child: SizedBox(
-                  width: size.width * 0.31,
-                  // width: double.infinity,
-                  height: 150,
-                  child: Center(child: Text('5th card')),
-                ),
-              ),
-            ),
-            Card(
-              color: Colors.teal[300],
-              child: InkWell(
-                splashColor: Colors.blue.withAlpha(30),
-                onTap: () {
-                  debugPrint('Card tapped.');
-                },
-                child: SizedBox(
-                  width: size.width * 0.31,
-                  // width: double.infinity,
-                  height: 150,
-                  child: Center(child: Text('6th card')),
-                ),
-              ),
-            ),
-          ],
-        )),
-        SizedBox(height: 50),
-        Center(
-          child: ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              primary: Color.fromARGB(255, 16, 255, 203), // background
-              onPrimary: Colors.white, // foreground
-            ),
-            onPressed: () async {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => MainPage(),
-                ),
-              );
-            },
-            child: const Text(
-              "Bluetooth",
-              style: TextStyle(
-                  fontSize: 12,
-                  //  fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(255, 0, 0, 0)),
-            ),
-          ),
-        ),
-      ]),
-      Center(child: Text('Health')),
-      Center(child: Text('Profile')),
-    ];
-
+    final double height = MediaQuery.of(context).size.height;
+    final double width = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        title: Text('HOSPICARE'),
-        elevation: 0,
-        centerTitle: true,
-      ),
-      body: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: screens[currentIndex]),
-      bottomNavigationBar: CustomBottomBoxBar(
-        onIndexChange: (int val) {
-          setState(() {
-            currentIndex = val;
-          });
-        },
-        inicialIndex: currentIndex,
-        items: [
-          CustomBottomBoxBarItem(Icons.home, Text('Home')),
-          CustomBottomBoxBarItem(Icons.history_rounded, Text('Logs')),
-          CustomBottomBoxBarItem(Icons.person, Text('Profile')),
-          // CustomBottomBoxBarItem(Icons.work_outline_rounded, Text('Work')),
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Container(
+            height: 230,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(50),
+                ),
+                color: Colors.blue),
+            child: Stack(
+              children: [
+                Positioned(
+                    top: 80,
+                    left: 0,
+                    child: Container(
+                        height: 100,
+                        width: 300,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            bottomRight: Radius.circular(50),
+                            topRight: Radius.circular(50),
+                          ),
+                        ))),
+                Positioned(
+                  top: 110,
+                  left: 20,
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      textStyle: const TextStyle(fontSize: 20),
+                    ),
+                    onPressed: () async {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => MainPage(),
+                        ),
+                      );
+                    },
+                    child: const Text(
+                      'Enable Bluetooth',
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
         ],
       ),
-
-      // bottomNavigationBar: BottomNavigationBar(
-
-      //   currentIndex: currentIndex,
-      //   onTap: (index) => setState(
-      //     () => currentIndex = index,
-      //   ),
-      //   iconSize: 35,
-      //   items: [
-      //     BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'
-      //         // backgroundColor: Colors.blue[300],
-      //         ),
-      //     BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'Health'
-      //         // backgroundColor: Colors.blue[300],
-      //         ),
-      //     BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'
-      //         // backgroundColor: Colors.blue[300],
-      //         ),
-      // ],
     );
-    // );
   }
 }
+
+// Size size = MediaQuery.of(context).size;
+    // var screens = [
+    //   ListView(children: <Widget>[
+    //     Container(
+    //       padding: const EdgeInsets.symmetric(vertical: 30),
+    //       width: double.infinity,
+    //       // height: double.infinity,
+    //       child: Center(child: Text('Hello Sriram')),
+    //     ),
+    //     SizedBox(height: 50),
+    //     // Center(
+    //     //     child: Row(
+    //     //   children: <Widget>[
+    //     //     Card(
+    //     //       color: Colors.teal[300],
+    //     //       child: InkWell(
+    //     //         splashColor: Colors.blue.withAlpha(30),
+    //     //         onTap: () {
+    //     //           debugPrint('Card tapped.');
+    //     //         },
+    //     //         child: SizedBox(
+    //     //           // width: double.infinity,
+    //     //           width: size.width * 0.31,
+    //     //           height: 150,
+    //     //           child: Center(child: Text('1st card')),
+    //     //         ),
+    //     //       ),
+    //     //     ),
+    //     //     Card(
+    //     //       color: Colors.teal[200],
+    //     //       child: InkWell(
+    //     //         splashColor: Colors.blue.withAlpha(30),
+    //     //         onTap: () {
+    //     //           debugPrint('Card tapped.');
+    //     //         },
+    //     //         child: SizedBox(
+    //     //           width: size.width * 0.31,
+    //     //           // width: double.infinity,
+    //     //           height: 150,
+    //     //           child: Center(child: Text('2nd card')),
+    //     //         ),
+    //     //       ),
+    //     //     ),
+    //     //     Card(
+    //     //       color: Colors.teal[300],
+    //     //       child: InkWell(
+    //     //         splashColor: Colors.blue.withAlpha(30),
+    //     //         onTap: () {
+    //     //           debugPrint('Card tapped.');
+    //     //         },
+    //     //         child: SizedBox(
+    //     //           width: size.width * 0.31,
+    //     //           // width: double.infinity,
+    //     //           height: 150,
+    //     //           child: Center(child: Text('3rd card')),
+    //     //         ),
+    //     //       ),
+    //     //     )
+    //     //   ],
+    //     // )),
+    //     // Center(
+    //     //     child: Row(
+    //     //   children: <Widget>[
+    //     //     Card(
+    //     //       color: Colors.teal[300],
+    //     //       child: InkWell(
+    //     //         splashColor: Colors.blue.withAlpha(30),
+    //     //         onTap: () {
+    //     //           debugPrint('Card tapped.');
+    //     //         },
+    //     //         child: SizedBox(
+    //     //           // width: double.infinity,
+    //     //           width: size.width * 0.31,
+    //     //           height: 150,
+    //     //           child: Center(child: Text('4th card')),
+    //     //         ),
+    //     //       ),
+    //     //     ),
+    //     //     Card(
+    //     //       color: Colors.teal[200],
+    //     //       child: InkWell(
+    //     //         splashColor: Colors.blue.withAlpha(30),
+    //     //         onTap: () {
+    //     //           debugPrint('Card tapped.');
+    //     //         },
+    //     //         child: SizedBox(
+    //     //           width: size.width * 0.31,
+    //     //           // width: double.infinity,
+    //     //           height: 150,
+    //     //           child: Center(child: Text('5th card')),
+    //     //         ),
+    //     //       ),
+    //     //     ),
+    //     //     Card(
+    //     //       color: Colors.teal[300],
+    //     //       child: InkWell(
+    //     //         splashColor: Colors.blue.withAlpha(30),
+    //     //         onTap: () {
+    //     //           debugPrint('Card tapped.');
+    //     //         },
+    //     //         child: SizedBox(
+    //     //           width: size.width * 0.31,
+    //     //           // width: double.infinity,
+    //     //           height: 150,
+    //     //           child: Center(child: Text('6th card')),
+    //     //         ),
+    //     //       ),
+    //     //     ),
+    //     //   ],
+    //     // )),
+    //     SizedBox(height: 50),
+    //     Center(
+    //       child: ElevatedButton(
+    //         style: ElevatedButton.styleFrom(
+    //           primary: Color.fromARGB(255, 16, 255, 203), // background
+    //           onPrimary: Colors.white, // foreground
+    //         ),
+    //         onPressed: () async {
+    //           Navigator.push(
+    //             context,
+    //             MaterialPageRoute(
+    //               builder: (context) => MainPage(),
+    //             ),
+    //           );
+    //         },
+    //         child: const Text(
+    //           "Bluetooth",
+    //           style: TextStyle(
+    //               fontSize: 12,
+    //               //  fontWeight: FontWeight.bold,
+    //               color: Color.fromARGB(255, 0, 0, 0)),
+    //         ),
+    //       ),
+    //     ),
+    //   ]),
+    //   Center(child: Text('Health')),
+    //   Center(child: Text('Profile')),
+    // ];
+
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: Text('HOSPICARE'),
+    //     elevation: 0,
+    //     centerTitle: true,
+    //   ),
+    //   body: Container(
+    //       width: MediaQuery.of(context).size.width,
+    //       height: MediaQuery.of(context).size.height,
+    //       child: screens[currentIndex]),
+    //   bottomNavigationBar: CustomBottomBoxBar(
+    //     onIndexChange: (int val) {
+    //       setState(() {
+    //         currentIndex = val;
+    //       });
+    //     },
+    //     inicialIndex: currentIndex,
+    //     items: [
+    //       CustomBottomBoxBarItem(Icons.home, Text('Home')),
+    //       CustomBottomBoxBarItem(Icons.history_rounded, Text('Logs')),
+    //       CustomBottomBoxBarItem(Icons.person, Text('Profile')),
+    //       // CustomBottomBoxBarItem(Icons.work_outline_rounded, Text('Work')),
+    //     ],
+    //   ),
+
+    //   // bottomNavigationBar: BottomNavigationBar(
+
+    //   //   currentIndex: currentIndex,
+    //   //   onTap: (index) => setState(
+    //   //     () => currentIndex = index,
+    //   //   ),
+    //   //   iconSize: 35,
+    //   //   items: [
+    //   //     BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'
+    //   //         // backgroundColor: Colors.blue[300],
+    //   //         ),
+    //   //     BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'Health'
+    //   //         // backgroundColor: Colors.blue[300],
+    //   //         ),
+    //   //     BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'
+    //   //         // backgroundColor: Colors.blue[300],
+    //   //         ),
+    //   // ],
+    // );
+    // // );
